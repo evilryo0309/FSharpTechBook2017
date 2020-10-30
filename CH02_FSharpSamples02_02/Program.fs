a2b65e0bed4654826a2a4d6049083c885193b51e
何俊良 <evilryo0309@gmail.com>
2020-10-30 22:08:07 +0800

    加入專案檔案。

---
+++ CH02_FSharpSamples02_02/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+//* ********************************
+//* 函數應用範例:
+//* ********************************
+open System
+
+//* 宣告函數 Multiple_Values_Plus()
+//* ********************************
+let Multiple_Values_Plus(v1:int, v2:int, v3:int, v4:int, v5:int) = 
+    let r = v1 + v2 + v3 + v4 + v5;
+    r;;     //* 函數回傳值    //* 函數結束
+//* ********************************
+
+Console.Write("呼叫函數 1, 計算 100 + 300 + 500 + 700 + 900 = ")
+Console.WriteLine(
+    Convert.ToString(
+        Multiple_Values_Plus(100, 300, 500, 700, 900)));
+Console.WriteLine(Console.ReadLine());;
