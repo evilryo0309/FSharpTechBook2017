00dd23111356b3cd9a431342bfaad85b4f5e18db
何俊良 <evilryo0309@gmail.com>
2020-11-08 23:21:57 +0800

    Add project CH02_FSharpSamples08_01

---
+++ CH02_FSharpSamples08_01/Program.fs
@@ -0,0 +1,22 @@
+﻿// **************************************
+// Visual F# 2017 遞迴函數設計
+// **************************************
+open System
+
+// **************************************
+// 使用遞迴函數計算費伯納西數列
+// **************************************
+let rec Fibonacci index : int32 = 
+    if index <= 2 then
+        // 當計算項數為第一與第二時，停止遞迴，傳回 1
+        1
+    else
+        // 當計算項數不為第一與第二時，執行遞迴呼叫
+        Fibonacci (index - 2) + 
+        Fibonacci (index - 1)
+
+// 1 1 2 3 5 8 13 21 34 55
+Console.WriteLine(
+    "費伯納西數列第 10 項為 " + Convert.ToString(Fibonacci(10)))
+
+Console.WriteLine(Console.ReadLine())
